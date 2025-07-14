a85d2be63ec2e1b8a320222a9ac884e1ae965903
Aditya Kumar <114330303+Kodeitya@users.noreply.github.com>
2025-07-14 14:20:51 +0530

    d

--- src/auth.js
+++
@@ -1,9 +0,0 @@
-export const getToken = () => localStorage.getItem("token");
-export const getRole = () => localStorage.getItem("role");
-export const setAuth = (token, role) => {
-  localStorage.setItem("token", token);
-  localStorage.setItem("role", role);
-};
-export const logout = () => {
-  localStorage.clear();
-};
